122f37da806a62e0b5e5b9d0fe08dcac3ec4dda2
Pritam Biswas <pritam.biswas@monstar-lab.com>
2024-07-08 21:57:35 +0200

    Initial Commit

---
+++ github_users/github_usersApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_usersApp.swift
+//  github_users
+//
+//  Created by Pritam Biswas on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct github_usersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
