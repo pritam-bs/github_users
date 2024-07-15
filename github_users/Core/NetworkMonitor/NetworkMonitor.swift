992bffb4fbe8c3728543fbc552f46fb29f8b4ca3
Pritam Biswas <pritam.biswas@monstar-lab.com>
2024-07-15 09:11:09 +0200

    Retry failed image download when network reconnected

--- github_users/Core/NetworkMonitor/NetworkMonitor.swift
+++ github_users/Core/NetworkMonitor/NetworkMonitor.swift
@@ -5,10 +5,6 @@
 //  Created by Pritam Biswas on 14.07.2024.
 //
 
-import Combine
-import Network
-
-import Foundation
 import Network
 import Combine
 
@@ -16,14 +12,15 @@ class NetworkMonitor: ObservableObject {
     private var monitor: NWPathMonitor
     private var queue: DispatchQueue
     @Published var isConnected: Bool = false
-
+    
     init() {
         self.monitor = NWPathMonitor()
         self.queue = DispatchQueue.global(qos: .background)
         self.monitor.pathUpdateHandler = { [weak self] path in
+            guard let self = self else { return }
             let status = path.status == .satisfied
             DispatchQueue.main.async {
-                self?.isConnected = status
+                self.isConnected = status
             }
         }
         self.monitor.start(queue: self.queue)
@@ -33,4 +30,5 @@ class NetworkMonitor: ObservableObject {
         self.monitor.cancel()
     }
 }
+
 
